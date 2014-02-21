29c16d46867b338ce412252f29c12c77c5fe5150
Steven Birkner <stevebirk36@gmail.com>
2014-02-21 14:17:04 -0500

    ADD: two new tests

--- test/test.js
+++ test/test.js
@@ -12,6 +12,22 @@ describe("Tags", function() {
             expect(results).to.have.a.property("depth", 4);
             expect(results).to.have.a.property("hello", "world");
 		});
+		it("Should fallback to defaults", function() {
+			var args = ["--depths=4", "--hello=world"];
+			var defaults = {depth: 2, foo: "bar"};
+			var results = tags.parse(args,defaults);
+
+			var expected = {
+				depths: 4,
+				hello: "world"
+			};
+			expect(results).to.deep.equal(expected);
+		});
+		it("should accept tags without values as a bool", function(){
+			var args = ["--searchContents"];
+			var results = tags.parse(args);
+			expect(results).to.have.a.property("searchContents", true);
+		});
 	});
 });
 
