a12b3a7f88eca30e01776840de6e101ebbd89d9c
Steven Birkner <stevebirk36@gmail.com>
2014-02-21 14:47:25 -0500

    ADD: New test and func multiply, REMOVED: Commented out net tuts tutorial for now

--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@ var expect = require("chai").expect;
 var main = require("../lib/main.js");
 var tags = require("../lib/tags.js");
 
+/*
 describe("Tags", function() {
 	describe("#parse()", function() {
 		it("SHould parse long formed tags", function() {
@@ -19,6 +20,7 @@ describe("Tags", function() {
 
 			var expected = {
 				depths: 4,
+				foo: "bar",
 				hello: "world"
 			};
 			expect(results).to.deep.equal(expected);
@@ -28,15 +30,40 @@ describe("Tags", function() {
 			var results = tags.parse(args);
 			expect(results).to.have.a.property("searchContents", true);
 		});
+		it("should accept short formed tags", function(){
+			var args = ["-sd=4", "-h"];
+			var replacements = {
+				s: "searchContents",
+				d: "depth",
+				h: "hello"
+			};
+			var results = tags.parse(args, {}, replacements);
+
+			var expected = {
+				searchContents: true,
+				depth: 4,
+				hello: true
+			};
+			expect(results).to.deep.equal(expected);
+		});
 	});
-});
+}); */
 
 describe("Main",function() {
 	describe("#add()", function() {
 		it("Should add two numbers", function() {
 			var result1 = main.add(4, 6);
+			var result2 = main.add(4, 6);
 
 			expect(result1).to.equal(10);
+		});
+	});
+	describe("multiply()", function() {
+		it("Should multiply 2 numbers", function() {
+			var result1 = main.multiply(4,5);
+			var result2 = main.multiply(3,4);
+
+			expect(result1).to.equal(20);
 		});
 	});
 
