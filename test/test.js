78678a83ce8909200e895d4586f74fcd9f6f1cca
Steven Birkner <stevebirk36@gmail.com>
2014-02-21 13:56:22 -0500

    ADD: Tags Test

--- test/test.js
+++ test/test.js
@@ -1,9 +1,23 @@
 var expect = require("chai").expect;
+
 var main = require("../lib/main.js");
+var tags = require("../lib/tags.js");
+
+describe("Tags", function() {
+	describe("#parse()", function() {
+		it("SHould parse long formed tags", function() {
+			var args = ["--depth=4", "--hello=world"];
+            var results = tags.parse(args);
+
+            expect(results).to.have.a.property("depth", 4);
+            expect(results).to.have.a.property("hello", "world");
+		});
+	});
+});
 
 describe("Main",function() {
-	describe("ADD", function() {
-		it("Add two numbers", function() {
+	describe("#add()", function() {
+		it("Should add two numbers", function() {
 			var result1 = main.add(4, 6);
 
 			expect(result1).to.equal(10);
